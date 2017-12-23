40ff4ecd993a8ce19ece02248e010775dccfc3de
mmnabi <mmnabi.cse@gmail.com>
2017-12-23 14:03:41 +0600

    Main Activity Modified

--- app/src/main/java/com/lubdhok/biyeshadi/MainActivity.java
+++ app/src/main/java/com/lubdhok/biyeshadi/MainActivity.java
@@ -2,12 +2,42 @@ package com.lubdhok.biyeshadi;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    Button btnLogIn, btnSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initViews();
+        initListeners();
+    }
+
+    private void initListeners() {
+        btnLogIn.setOnClickListener(this);
+        btnSignUp.setOnClickListener(this);
+    }
+
+    private void initViews() {
+        btnLogIn = findViewById(R.id.btnLogIn);
+        btnSignUp = findViewById(R.id.btnSignUp);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btnLogIn:
+                // Load login activity
+                break;
+
+            case R.id.btnSignUp:
+                // Load sign up activity
+                break;
+        }
     }
 }
